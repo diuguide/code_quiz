ff4c498b8bb8662b35fe8aa470de154001e8f880
diuguide <61917141+diuguide@users.noreply.github.com>
2020-06-09 13:55:03 -0400

    added highscore page and localstorage

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const lowerBoard = document.getElementById("lowerboard");
 const submitName = document.getElementById("submitname");
 const submitBtn = document.getElementById("submitbtn");
 const highScores = document.getElementById("highscores");
+const resetBtn = document.getElementById("resetbtn");
 
 let questions = [
   {
@@ -49,7 +50,7 @@ let questions = [
 
 const lastQuestion = questions.length -1;
 let runningQuestion = 0;
-let count = 150;
+let count = 75;
 let score = 0;
 let clock;
 
@@ -66,23 +67,27 @@ function renderCounter() {
       counter.innerHTML = "Time Remaining: " + count;
       count--;
       if(count == 0) {
-        alert("You Lose!")
+        finalScore();
       }
 }
-
-renderQuestion();
-clock = setInterval(renderCounter,1000);
+start.addEventListener('click', startQuiz);
+function startQuiz() {
+  start.style.display = "none";
+  renderQuestion();
+  renderCounter();
+  quizBox.style.display = "block";
+  lowerBoard.style.display = "block";
+  clock = setInterval(renderCounter,1000);
+  }
 
 function checkAnswer(answer) {
   if(answer == questions[runningQuestion].correct) {
-      score++;
-      count = count + 10;
-      alert("answer is correct");
+      score = score + 500;
+      alert("Correct!");
       scoreRender();    
   }else {
-      score--;
-      count = count - 10;
-      alert("answer is wrong");
+      count = count - 25;
+      alert("Wrong!");
       scoreRender();
   }
   if(runningQuestion < lastQuestion) {
@@ -98,18 +103,21 @@ function checkAnswer(answer) {
 
 function scoreRender() {
     scoreBoard.style.display = "block";
-    const currentScore = score;
     scoreBoard.innerHTML = "CURRENT SCORE: " + score;
 }
 
 function finalScore() {
-  scoreBoard.style.display = "none";
-  counter.style.display = "none";
-  lowerBoard.style.display = "none";
   submitScore.style.display = "block";
-  quizBox.innerHTML = "<h1>Your Final Score is: " + score; 
+  resetBtn.style.display = "block";
+  lowerBoard.style.display = "none";
+  quizBox.innerHTML = "<h1>GAME OVER! Score: " + score;
+  localStorage.setItem('score', score);
 }
-
-function renderHighscores() {
-  highScores.innerHTML = submitName + ":" + score;
-}
+submitBtn.addEventListener('click', function renderHighscores() {
+  event.preventDefault();
+  submitScore.style.display = "none";
+  quizBox.style.display = "none";
+  let name = prompt('Please enter your initials');
+  localStorage.setItem('name', name);
+  highScores.innerHTML = "<h1>HIGH SCORES</h1><hr><h2>" + localStorage.getItem('name') + ":" + localStorage.getItem('score') + "</h2>";  
+});
