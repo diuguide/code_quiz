8312341ef9af775a240764c0cfcb55a0d659ee35
diuguide <61917141+diuguide@users.noreply.github.com>
2020-06-09 19:32:46 -0400

    fixed an error

--- script.js
+++ script.js
@@ -18,37 +18,41 @@ const resetBtn = document.getElementById("reset");
 
 let questions = [
   {
-      question: "Which of these is not a programming language?",
-      choiceA: "html",
-      choiceB: "javascript",
-      choiceC: "poothon",
-      choiceD: "php",
-      correct: "C"
-  },{
-      question: "Which of the following is a commonly used data type?",
-      choiceA: "bergs",
-      choiceB: "beans",
-      choiceC: "bits",
-      choiceD: "boops",
-      correct: "C"
-  },{
-      question: "What is the command to clone a github repository to your local hard drive?",
-      choiceA: "gitgrab",
-      choiceB: "up_clone",
-      choiceC: "git clone",
-      choiceD: "server HTTPS request pull action",
-      correct: "C"
-  },{
-      question: "What is the total data mass of the internet in bytes?",
-      choiceA: "18 mebibytes",
-      choiceB: "18 zettabytes",
-      choiceC: "18 yottabytes",
-      choiceD: "18 petabytes",
-      correct: "B"
-  }
+    question: "Which of these is not a programming language?",
+    choiceA: "html",
+    choiceB: "javascript",
+    choiceC: "poothon",
+    choiceD: "php",
+    correct: "C",
+  },
+  {
+    question: "Which of the following is a commonly used data type?",
+    choiceA: "bergs",
+    choiceB: "beans",
+    choiceC: "bits",
+    choiceD: "boops",
+    correct: "C",
+  },
+  {
+    question:
+      "What is the command to clone a github repository to your local hard drive?",
+    choiceA: "gitgrab",
+    choiceB: "up_clone",
+    choiceC: "git clone",
+    choiceD: "server HTTPS request pull action",
+    correct: "C",
+  },
+  {
+    question: "What is the total data mass of the internet in bytes?",
+    choiceA: "18 mebibytes",
+    choiceB: "18 zettabytes",
+    choiceC: "18 yottabytes",
+    choiceD: "18 petabytes",
+    correct: "B",
+  },
 ];
 
-const lastQuestion = questions.length -1;
+const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
 let count = 75;
 let score = 0;
@@ -64,62 +68,66 @@ function renderQuestion() {
 }
 
 function renderCounter() {
-      counter.innerHTML = "Time Remaining: " + count;
-      count--;
-      if(count <= 0) {
-        finalScore();
-      }
+  counter.innerHTML = "Time Remaining: " + count;
+  count--;
+  if (count <= 0) {
+    finalScore();
+  }
 }
-start.addEventListener('click', startQuiz);
+start.addEventListener("click", startQuiz);
 function startQuiz() {
   start.style.display = "none";
   renderQuestion();
   renderCounter();
   quizBox.style.display = "block";
   lowerBoard.style.display = "block";
-  clock = setInterval(renderCounter,1000);
-  }
+  clock = setInterval(renderCounter, 1000);
+}
 
 function checkAnswer(answer) {
-  if(answer == questions[runningQuestion].correct) {
-      score = score + 500;
-      alert("Correct!");
-      scoreRender();    
-  }else {
-      count = count - 25;
-      alert("Wrong!");
-      scoreRender();
+  if (answer == questions[runningQuestion].correct) {
+    score = score + 500;
+    alert("Correct!");
+    scoreRender();
+  } else {
+    count = count - 25;
+    alert("Wrong!");
+    scoreRender();
   }
-  if(runningQuestion < lastQuestion) {
-      runningQuestion++;
-      renderQuestion();
-  }else {
+  if (runningQuestion < lastQuestion) {
+    runningQuestion++;
+    renderQuestion();
+  } else {
     finalScore();
     clearInterval(clock);
   }
 }
 
 function scoreRender() {
-    scoreBoard.style.display = "block";
-    scoreBoard.innerHTML = "CURRENT SCORE: " + score;
+  scoreBoard.style.display = "block";
+  scoreBoard.innerHTML = "CURRENT SCORE: " + score;
 }
 
 function finalScore() {
   submitScore.style.display = "block";
   lowerBoard.style.display = "none";
   quizBox.innerHTML = "<h1>GAME OVER! Score: " + score;
-  localStorage.setItem('score', score);
+  localStorage.setItem("score", score);
 }
-submitBtn.addEventListener('click', function renderHighscores() {
+submitBtn.addEventListener("click", function renderHighscores() {
   event.preventDefault();
   submitScore.style.display = "none";
   quizBox.style.display = "none";
-  let name = prompt('Please enter your initials');
-  localStorage.setItem('name', name);
-  highScores.innerHTML = "<h1>HIGH SCORES</h1><hr><h2>" + localStorage.getItem('name') + ":" + localStorage.getItem('score') + "</h2>";
-  resetBtn.style.display = "block"
-  resetBtn.addEventListener('click', function myFunction() {
-  location.replace("index.html")
-});  
+  let name = prompt("Please enter your initials");
+  localStorage.setItem("name", name);
+  highScores.innerHTML =
+    "<h1>HIGH SCORES</h1><hr><h2>" +
+    localStorage.getItem("name") +
+    ":" +
+    localStorage.getItem("score") +
+    "</h2>";
+  resetBtn.style.display = "block";
+  resetBtn.addEventListener("click", function myFunction() {
+    location.replace("index.html");
+  });
 });
-
