e426ebfb7ab7c5e36f155eb9b67dc749460ce71d
diuguide <61917141+diuguide@users.noreply.github.com>
2020-06-11 12:39:18 -0400

    added comments to js file, color and other minor changes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// pull elements from HTML //
+
 const start = document.getElementById("start");
 const stop = document.getElementById("stop");
 const highscores = document.getElementById("highscores");
@@ -15,6 +17,8 @@ const submitName = document.getElementById("submitname");
 const submitBtn = document.getElementById("submitbtn");
 const highScores = document.getElementById("highscores");
 const resetBtn = document.getElementById("reset");
+const title = document.getElementById("titlebox");
+// create an array of questions //
 
 let questions = [
   {
@@ -51,13 +55,29 @@ let questions = [
     correct: "B",
   },
 ];
-
+// create variable to signal last question //
 const lastQuestion = questions.length - 1;
+// variable for current question //
 let runningQuestion = 0;
+// starting count for clock //
 let count = 75;
+// starting count for score //
 let score = 0;
+// set clock variable //
 let clock;
 
+// event listener for start button, then renders question, starts clock, hides start button, shows quiz and scoreboard, starts clock //
+start.addEventListener("click", startQuiz);
+function startQuiz() {
+  title.style.display = "none";
+  start.style.display = "none";
+  renderQuestion();
+  renderCounter();
+  quizBox.style.display = "block";
+  lowerBoard.style.display = "block";
+  clock = setInterval(renderCounter, 1000);
+}
+// function to render question, pulls from array of questions, prints question and choices to html //
 function renderQuestion() {
   let q = questions[runningQuestion];
   question.innerHTML = "<p>" + q.question + "</p>";
@@ -66,7 +86,7 @@ function renderQuestion() {
   choiceC.innerHTML = q.choiceC;
   choiceD.innerHTML = q.choiceD;
 }
-
+// displays timer, ends game when time is up //
 function renderCounter() {
   counter.innerHTML = "Time Remaining: " + count;
   count--;
@@ -74,16 +94,7 @@ function renderCounter() {
     finalScore();
   }
 }
-start.addEventListener("click", startQuiz);
-function startQuiz() {
-  start.style.display = "none";
-  renderQuestion();
-  renderCounter();
-  quizBox.style.display = "block";
-  lowerBoard.style.display = "block";
-  clock = setInterval(renderCounter, 1000);
-}
-
+// checks question array for correct answer, changes score, adjusts time if question is wrong, moves to next question, or ends game and shows score //
 function checkAnswer(answer) {
   if (answer == questions[runningQuestion].correct) {
     score = score + 500;
@@ -103,17 +114,21 @@ function checkAnswer(answer) {
   }
 }
 
+// renders score board, shows current score //
 function scoreRender() {
   scoreBoard.style.display = "block";
   scoreBoard.innerHTML = "CURRENT SCORE: " + score;
 }
-
+// hides quiz and score board, displays final score and high score submit button //
 function finalScore() {
   submitScore.style.display = "block";
   lowerBoard.style.display = "none";
   quizBox.innerHTML = "<h1>GAME OVER! Score: " + score;
   localStorage.setItem("score", score);
 }
+
+// event listener for high score button, prompts user for initials, stores information to local storage, then displays high scores from localstorage //
+
 submitBtn.addEventListener("click", function renderHighscores() {
   event.preventDefault();
   submitScore.style.display = "none";
